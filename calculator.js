5dc722e1212776a89915ab86928267afdd9ee2d4
pajith551@gmail.com <pajith551@gmail.com>
2022-03-08 17:44:12 +0530

    calculator

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
 const express = require("express");
-
+const bodyParser = require("body-parser")
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.send("hello");
-})
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    // console.log(req.body);
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+    res.send("Success " + result);
+});
 
 app.listen(3000, function(){
     console.log("App started");
